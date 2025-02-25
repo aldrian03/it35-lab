b5aa25592c89804087293b93966b448d7616fc7f
aldrian03 <cayomocjastin@gmail.com>
2025-02-25 14:36:57 +0800

    Updates

--- assets/ios.transition-legacy-DfxdIoBz.js
+++ assets/ios.transition-legacy-DfxdIoBz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DW8w_ABe.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BGJKX9Ni.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-BHHToE7c.js
+++ assets/keyboard2-legacy-BHHToE7c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DW8w_ABe.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BGJKX9Ni.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
