cee98fb3da9ce424353c99b30c7495d43172af89
aldrian03 <cayomocjastin@gmail.com>
2025-02-24 22:49:18 -0400

    minimal changes app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 import Login from './pages/Login';
-import Menu from './pages/Menu';
+import Menu from './pages/Login'
 
 setupIonicReact();
 
--- vite.config.ts
+++ vite.config.ts
@@ -17,3 +17,4 @@ export default defineConfig({
     setupFiles: './src/setupTests.ts',
   }
 })
+
