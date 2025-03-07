499990339c9b434d661a34449020b231d00791d1
aldrian03 <cayomocjastine@gmail.com>
2025-03-07 22:52:43 +0800

    Updates

--- assets/swipe-back-legacy--pbx5-f8.js
+++ assets/swipe-back-legacy--pbx5-f8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B0MkyQZW.js","./index3-legacy-BASkiipy.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BCA205rw.js","./index3-legacy-DKkkkGAo.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
