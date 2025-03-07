a9a60ccf40d519ff3968eee7c502c652c70adaf4
aldrian03 <cayomocjastine@gmail.com>
2025-03-07 22:51:32 +0800

    Added new features at login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useRef } from 'react';
 import {
   IonButton,
   IonButtons,
@@ -11,26 +10,15 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  useIonRouter,
-  IonCheckbox
+  useIonRouter
 } from '@ionic/react';
-
 import { personCircleOutline } from 'ionicons/icons';
-
 function Login() {
   const navigation = useIonRouter();
   const doLogin = () => {
     navigation.push('/it35-lab/app', 'forward', 'replace');
   };
-
-  const ref = useRef<HTMLAnchorElement>(null);
-
-  useEffect(() => {
-    ref.current?.addEventListener('click', (event) => {
-      event.stopPropagation();
-    });
-  }, [ref]);
-
+  
   return (
     <IonPage>
       <IonHeader>
@@ -42,29 +30,21 @@ function Login() {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-          <IonIcon icon={personCircleOutline} style={{ fontSize: '80px' }} />
-        </div>
-        <IonItem>
-          <IonInput label='Username' placeholder='Enter Username'></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonInput label='Password' type='password' placeholder='Enter Password'></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonCheckbox>
-            I agree to the{' '}
-            <a href='#' ref={ref}>
-              terms and conditions
-            </a>
-          </IonCheckbox>
-        </IonItem>
-        <IonButton onClick={() => doLogin()} expand='full'>
+      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+        <IonIcon icon={personCircleOutline} style={{ fontSize: '80px' }} />
+      </div>
+      <IonItem>
+        <IonInput label="Username" placeholder="Enter Username"></IonInput>
+      </IonItem>
+      <IonItem>
+        <IonInput label="Password" type="password" placeholder="Enter Password"></IonInput>
+      </IonItem>
+        <IonButton onClick={() => doLogin()} expand="full">
           Login
         </IonButton>
       </IonContent>
     </IonPage>
   );
 }
-
-export default Login;
+  
+  export default Login;
