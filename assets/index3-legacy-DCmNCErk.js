d542535dc469e022333d09b1453ef13b772dc948
aldrian03 <cayomocjastine@gmail.com>
2025-03-07 23:06:04 +0800

    Updates

--- assets/index3-legacy-DCmNCErk.js
+++ assets/index3-legacy-DCmNCErk.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QFBjCGgF.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-Dnh3a7BU.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-CmGNtAPa.js
+++ assets/swipe-back-legacy-CmGNtAPa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QFBjCGgF.js","./index3-legacy-cJbzsJNW.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Dnh3a7BU.js","./index3-legacy-DCmNCErk.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
