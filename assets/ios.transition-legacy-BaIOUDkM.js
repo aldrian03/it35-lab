1e5ba87bd4d9e6663354ff43d3d714117d52f263
aldrian03 <cayomocjastin@gmail.com>
2025-05-01 12:10:36 +0800

    Updates

--- assets/ios.transition-legacy-BaIOUDkM.js
+++ assets/ios.transition-legacy-BaIOUDkM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dnh3a7BU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-PJQ-PqYS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
