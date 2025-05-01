46e6d6436f22813edafcb9d964629715febfd3e5
aldrian03 <cayomocjastin@gmail.com>
2025-05-01 12:07:22 +0800

    fix feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,36 +1,14 @@
-import React from 'react'; 
 import { 
-  IonButton, 
-  IonCard, 
-  IonCardContent, 
-  IonCardHeader, 
-  IonCardSubtitle, 
-  IonCardTitle, 
   IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonMenuButton, 
-  IonPage, 
-  IonTitle, 
-  IonToolbar 
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
-const Example: React.FC = () => {
-  return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Welcome! buyers.</IonCardTitle>
-        <IonCardSubtitle>Buy now.</IonCardSubtitle>
-      </IonCardHeader>
-      <IonCardContent>
-       An e-commerce platform for school supplies.
-      </IonCardContent>
-
-      <IonButton fill="clear">Action 1</IonButton>
-      <IonButton fill="clear">Action 2</IonButton>
-    </IonCard>
-  );
-};
 
 const Feed: React.FC = () => {
   return (
@@ -45,6 +23,7 @@ const Feed: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
       
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
