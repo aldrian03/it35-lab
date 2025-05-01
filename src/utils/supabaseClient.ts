52368a14667c27fe0d2b66464ca74af27ce9a843
aldrian03 <cayomocjastin@gmail.com>
2025-05-01 11:51:14 +0800

    set up api

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yrztgeegweciulbyhdwt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyenRnZWVnd2VjaXVsYnloZHd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NjAzMzksImV4cCI6MjA1ODQzNjMzOX0.Kx4RFs-85AbnULQn2fMKn-jUgsuJM-kxwQzFxt8RDvo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
